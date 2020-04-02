d0ad9156b3c2e8b536c85abedd16ab6e02576ae6
Nathan Hill <nateohill@gmail.com>
2020-04-01 19:41:33 -0500

    Added imports of actions that will be needed Added some comments mapping out my thought process of how to build it out

--- src/screens/FamiliesList.tsx
+++ src/screens/FamiliesList.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Text, SafeAreaView } from "react-native";
+import { addFamily, removeFamily } from '../redux-modules/families/familySlice';
+import { Button } from 'react-native-elements';
+import { useSelector, useDispatch } from 'react-redux';
 
 
 const FamiliesList = ({ navigation }) => {
+  const state = useSelector(state => state);
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView>
       <Text>Families List</Text>
+      {/* Map out families here */}
+      <Button title="Add Family" onPress={() => { /* dispatch(addFamily({familyData})) */ }} />
+      <Button title="Delete Family" onPress={() => { /* dispatch(removeFamily({{familyId: familyId}})) */ }} />
     </SafeAreaView>
   );
 };

48ff3759b48d7c833290c0da7389c2f724f0e642
Nathan Hill <nateohill@gmail.com>
2020-04-01 19:48:36 -0500

    Changed imports to respect new exports of reducers from slices

--- src/redux-modules/store.ts
+++ src/redux-modules/store.ts
@@ -1,16 +1,24 @@
 import reduxToolkit from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
-import { counterSlice } from "./deacons";
-import { counterSagas } from "./deacons/sagas";
+import deaconReducer from "./deacons/deaconSlice";
+import { deaconSagas } from "./deacons/deaconSagas";
+import familyReducer from './families/familySlice';
+import { familySagas } from './families/familySagas';
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = reduxToolkit.configureStore({
-  reducer: { counterSlice: counterSlice.reducer },
+  reducer: {
+    deaconSlice: deaconReducer,
+    familySlice: familyReducer
+  },
   middleware: [sagaMiddleware]
 });
 
-const sagas = [...counterSagas];
+const sagas = [
+  ...deaconSagas,
+  ...familySagas
+];
 sagas.forEach(sagaMiddleware.run);
 
 export default store;

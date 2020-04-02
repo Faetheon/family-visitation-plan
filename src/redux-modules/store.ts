514bf021415b8a18b9359fea4450acbc722c8c3a
Nathan Hill <nateohill@gmail.com>
2020-04-01 19:50:10 -0500

    Added peopleReducer to the store Added peopleSagas to the sagas

--- src/redux-modules/store.ts
+++ src/redux-modules/store.ts
@@ -4,21 +4,26 @@ import deaconReducer from "./deacons/deaconSlice";
 import { deaconSagas } from "./deacons/deaconSagas";
 import familyReducer from './families/familySlice';
 import { familySagas } from './families/familySagas';
+import peopleReducer from "./people/peopleSlice";
+import { peopleSagas } from "./people/peopleSagas";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = reduxToolkit.configureStore({
   reducer: {
     deaconSlice: deaconReducer,
-    familySlice: familyReducer
+    familySlice: familyReducer,
+    peopleSlice: peopleReducer
   },
   middleware: [sagaMiddleware]
 });
 
 const sagas = [
   ...deaconSagas,
-  ...familySagas
+  ...familySagas,
+  ...peopleSagas
 ];
+
 sagas.forEach(sagaMiddleware.run);
 
 export default store;

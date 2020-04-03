17d57c70ccb253eae10db1663d20597fa56547e7
Nathan Hill <nateohill@gmail.com>
2020-04-03 16:18:05 -0500

    Changed passing the navigation object to useNavigation hook Made profile icon stick to the top right of the screen

--- src/components/ProfileButton.tsx
+++ src/components/ProfileButton.tsx
@@ -1,27 +1,28 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Button, Avatar } from 'react-native-elements';
-import { useSelector } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
 
 const styles = StyleSheet.create({
-  centeredContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
+  profileView: {
+    position: "absolute",
+    right: 5,
+    top: 50,
+    zIndex: 1
   }
 });
 
-const ProfileButton = ({ navigation }) => {
-  const counter = useSelector(state => state.counterSlice);
+const ProfileButton = () => {
+  const navigation = useNavigation();
 
   return (
-    <View style={styles.centeredContainer}>
+    <View style={styles.profileView}>
       {/* Photo by Joshua Fuller on Unsplash */}
       <Avatar
         size="small"
         rounded
         icon={{ name: 'user', type: 'font-awesome' }}
-        onPress={() => navigation.push("Profile")}
+        onPress={() => navigation.navigate("Profile")}
         activeOpacity={0.7}
       />
     </View>
